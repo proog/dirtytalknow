177045cdf3979ec6f913c99cd502f91f19e86d20
Per Mortensen <per@permortensen.com>
2020-06-28 17:24:29 +0200

    Add retry when getting comments

--- normalcommenter/dirty.py
+++ normalcommenter/dirty.py
@@ -1,12 +1,25 @@
 import logging
+
 import requests
+
+MAX_RETRIES = 2
 
 
 def get_comment() -> str:
-    logging.info("Requesting comment")
-    resp = requests.get("https://dirty.per.computer/porn")
-    resp.raise_for_status()
+    retries = 0
 
-    message = resp.json()["message"]
-    logging.info("Received message: %s", message)
-    return message
+    while retries <= MAX_RETRIES:
+        try:
+            logging.info("Requesting comment")
+            resp = requests.get("https://dirty.per.computer/porn")
+            resp.raise_for_status()
+
+            comment = resp.json()["message"]
+            logging.info("Received comment: %s", comment)
+            return comment
+        except requests.HTTPError:
+            logging.exception(
+                "HTTP error while getting comment. Retrying %i times.",
+                MAX_RETRIES - retries,
+            )
+            retries += 1
