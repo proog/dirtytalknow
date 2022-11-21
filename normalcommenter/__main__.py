500fd2026ad1e171b7572774587c89810820c05d
Per Mortensen <per@permortensen.com>
2022-11-21 20:03:32 +0100

    Add alt text

--- normalcommenter/__main__.py
+++ normalcommenter/__main__.py
@@ -31,7 +31,7 @@ while True:
 
         try:
             image = imaging.make_image_with_text(bg_image, comment, position=position)
-            twitter_api.tweet_image(image)
+            twitter_api.tweet_image(image, alt_text=comment)
         except imaging.TextFittingException:
             logging.exception("Making image failed, posting text instead")
             twitter_api.tweet_text(comment)
--- normalcommenter/twitter.py
+++ normalcommenter/twitter.py
@@ -12,7 +12,7 @@ class Twitter:
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
-    def tweet_text(self, text):
+    def tweet_text(self, text: str):
         text = " ".join(text.splitlines())
         tweets = textwrap.wrap(text, width=280)
         reply_to_id = None
@@ -23,7 +23,7 @@ class Twitter:
             reply_kwargs = {"in_reply_to_status_id": reply_to_id}
             reply_to_id = self.api.update_status(tweet, **reply_kwargs).id
 
-    def tweet_image(self, file, filename="image.jpg"):
+    def tweet_image(self, file, filename="image.jpg", alt_text=""):
         logging.info("Uploading media with filename %s", filename)
 
         # Seek to beginning before uploading https://github.com/tweepy/tweepy/issues/1667#issuecomment-927342823
@@ -31,6 +31,10 @@ class Twitter:
 
         media = self.api.media_upload(filename, file=file)
         media_id = media.media_id
+
+        if alt_text:
+            logging.info('Adding alt text "%s" to media id: %i', alt_text, media_id)
+            self.api.create_media_metadata(media_id, alt_text)
 
         logging.info("Tweeting media id: %i", media_id)
         self.api.update_status("", media_ids=[media_id])
