2b6e31ca469213e3f705d927c934319aade81bcc
Per Mortensen <per@permortensen.com>
2023-07-22 01:06:56 +0200

    Add image posting tests

--- test_mastodon.py
+++ test_mastodon.py
@@ -1,4 +1,6 @@
+import base64
 import os
+from io import BytesIO
 
 import pytest
 
@@ -18,3 +20,21 @@ def test_mastodon_post_text():
     text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque condimentum felis porttitor imperdiet ultricies. Duis eget ipsum nunc. Aenean vehicula faucibus risus, ac dapibus dui maximus a. Sed quis magna ex. Nam pharetra est facilisis tortor malesuada placerat nec at ex. Duis blandit lobortis maximus. Cras pretium sollicitudin sem eu tincidunt. Vestibulum ac nunc pharetra, tincidunt dolor in, tincidunt lectus. Proin viverra quis mauris at congue. Nunc mollis nunc sed felis porta, ut aliquam tortor dapibus nam."
 
     mastodon_api.post_text(text)
+
+
+@pytest.mark.skip()
+def test_mastodon_post_image():
+    mastodon_api = Mastodon(
+        os.environ["MASTODON_BASE_URL"],
+        os.environ["MASTODON_CLIENT_ID"],
+        os.environ["MASTODON_CLIENT_SECRET"],
+        os.environ["MASTODON_ACCESS_TOKEN"],
+    )
+
+    # A 200x200 dark square PNG
+    image_bytes = base64.b64decode(
+        "iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQAQMAAAC6caSPAAAAA1BMVEUfHx/wUtr8AAAAKklEQVR4Ae3BgQAAAADDoPtTH2AK1QAAAAAAAAAAAAAAAAAAAAAAAACAOE+wAAH1KUxJAAAAAElFTkSuQmCC"
+    )
+    image_stream = BytesIO(image_bytes)
+
+    mastodon_api.post_image(image_stream, alt_text="A dark square")
--- test_twitter.py
+++ test_twitter.py
@@ -1,4 +1,6 @@
+import base64
 import os
+from io import BytesIO
 
 import pytest
 
@@ -18,3 +20,21 @@ def test_twitter_tweet_text():
     text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque condimentum felis porttitor imperdiet ultricies. Duis eget ipsum nunc. Aenean vehicula faucibus risus, ac dapibus dui maximus a. Sed quis magna ex. Nam pharetra est facilisis tortor malesuada placerat nec at ex. Duis blandit lobortist."
 
     twitter_api.tweet_text(text)
+
+
+@pytest.mark.skip()
+def test_twitter_tweet_image():
+    twitter_api = Twitter(
+        os.environ["TWITTER_CONSUMER_KEY"],
+        os.environ["TWITTER_CONSUMER_SECRET"],
+        os.environ["TWITTER_ACCESS_TOKEN"],
+        os.environ["TWITTER_ACCESS_TOKEN_SECRET"],
+    )
+
+    # A 200x200 dark square PNG
+    image_bytes = base64.b64decode(
+        "iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQAQMAAAC6caSPAAAAA1BMVEUfHx/wUtr8AAAAKklEQVR4Ae3BgQAAAADDoPtTH2AK1QAAAAAAAAAAAAAAAAAAAAAAAACAOE+wAAH1KUxJAAAAAElFTkSuQmCC"
+    )
+    image_stream = BytesIO(image_bytes)
+
+    twitter_api.tweet_image(image_stream, alt_text="A dark square")
